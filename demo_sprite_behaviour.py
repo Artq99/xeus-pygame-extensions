a5897ed1f321ad55ee312d812d7e1d0634b7004e
Artq99 <amatracki@gmail.com>
2018-03-24 23:12:38 +0100

    Demo application for using SpriteBehaviour added.

---
+++ demo_sprite_behaviour.py
@@ -0,0 +1,53 @@
+import os
+
+import pygame
+from pygame.locals import *
+
+from xpgext.application import XPGApplication
+from xpgext.scene_manager import SimpleSceneManager
+from xpgext.scene import SimpleScene
+from xpgext.sprite import Sprite, SpriteBehaviour
+
+
+class DemoSpriteBehaviour(SpriteBehaviour):
+
+    def on_update(self):
+        print("Sprite update!")
+
+    def on_handle_event(self, event):
+        if event.type == MOUSEMOTION:
+            print("-- Mouse moved!")
+
+    def on_click(self, button):
+        print("-- -- Mouse button {} clicked!".format(button))
+
+    def on_hover(self):
+        print("-- -- Mouse enters!")
+
+    def on_hover_exit(self):
+        print("-- -- Mouse exits!")
+
+
+class DemoScene(SimpleScene):
+    def __init__(self, scene_manager):
+        super().__init__(scene_manager)
+
+        demo_sprite = Sprite(self.scene_manager)
+        demo_sprite.load_image(os.path.join("demo_resources", "star.png"))
+        demo_sprite.set_pos(270, 190)
+        demo_sprite.behaviour = DemoSpriteBehaviour(demo_sprite)
+        self.sprites.append(demo_sprite)
+
+
+class Application(XPGApplication):
+    def __init__(self):
+        super().__init__(SimpleSceneManager(), (640, 480))
+        self.scene_manager.register_scene(DemoScene, "demo")
+        self.scene_manager.load_scene("demo")
+
+
+if __name__ == "__main__":
+    pygame.init()
+    application = Application()
+    application.run_main_loop()
+    pygame.quit()
