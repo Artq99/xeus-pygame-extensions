12dc0f579775c7d4ab5ac51c4a5285aacccc8e7c
Artq99 <amatracki@gmail.com>
2018-03-24 21:57:03 +0100

    SpriteBehaviour added.

--- xpgext/sprite.py
+++ xpgext/sprite.py
@@ -35,3 +35,45 @@ class Sprite(pygame.sprite.Sprite):
 
     def set_pos(self, x, y):
         self.rect.topleft = (x, y)
+
+
+class SpriteBehaviour:
+    """
+    Base class for scripts controlling sprite behaviour.
+
+    Override the appropriate method and instantiate the descendant of this class
+    to Sprite.behaviourScript.
+    """
+
+    def __init__(self, sprite):
+        self.sprite = sprite
+
+    def on_update(self):
+        """Method called on Sprite.update."""
+
+    def on_handle_event(self, event):
+        """
+        Method called on Sprite.handle_event.
+
+        It is advised not to use this method to handle on_click, on_hover and on_hover_exit.
+        Override the corresponding methods instead. This method may return True if you wish
+        the game manager to stop checking if any sprite handled the given event after this call.
+        """
+
+    def on_click(self, button):
+        """
+        Method called on Sprite.handle_event, when the sprite has been clicked.
+
+        :param button: mouse button that fired this event
+        :type button: int
+        """
+
+    def on_hover(self):
+        """
+        Method called on Sprite.handle_event, when mouse pointer starts colliding with the sprite.
+        """
+
+    def on_hover_exit(self):
+        """
+        Method called on Sprite.handle_event, when mouse pointer stops colliding with the sprite.
+        """
