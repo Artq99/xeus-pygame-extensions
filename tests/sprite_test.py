3456191debd4ad9069aa2d58bedb940bc55ff20b
Artq99 <amatracki@gmail.com>
2018-06-27 19:29:56 +0200

    refactor the existing tests for XPGESprite class

--- tests/sprite_test.py
+++ tests/sprite_test.py
@@ -6,13 +6,31 @@ from pygame import Rect, Surface, USEREVENT, MOUSEMOTION
 
 from xpgext.sprite import XPGESprite, XPGEGroup, SpriteBehaviour
 
-MOUSE_POS = (50, 50)
-TEST_MOUSEMOTION_EVENT_WITH_POS_INSIDE_SPRITE = Event(MOUSEMOTION, {'pos': (50, 50)})
-TEST_MOUSEMOTION_EVENT_WITH_POS_OUTSIDE_SPRITE = Event(MOUSEMOTION, {'pos': (200, 200)})
+SPRITE_X = 0
+SPRITE_Y = 0
+SPRITE_SIZE_X = 100
+SPRITE_SIZE_Y = 100
+MOUSE_POS_INSIDE_SPRITE = (50, 50)
+MOUSE_POS_OUTSIDE_SPRITE = (200, 200)
+TEST_USEREVENT = Event(USEREVENT, dict())
+TEST_MOUSEMOTION_EVENT_WITH_POS_INSIDE_SPRITE = Event(MOUSEMOTION, {'pos': MOUSE_POS_INSIDE_SPRITE})
+TEST_MOUSEMOTION_EVENT_WITH_POS_OUTSIDE_SPRITE = Event(MOUSEMOTION, {'pos': MOUSE_POS_OUTSIDE_SPRITE})
 
 
 class XPGESpriteTest(TestCase):
     """Test class for XPGESprite class."""
+
+    def setUp(self):
+        self.sprite = XPGESprite(None)
+        self.sprite.rect = Rect(SPRITE_X, SPRITE_Y, SPRITE_SIZE_X, SPRITE_SIZE_Y)
+        self.sprite.image = Surface((SPRITE_SIZE_X, SPRITE_SIZE_Y))
+
+        self.component_1 = Mock(spec=SpriteBehaviour)
+        self.component_2 = Mock(spec=SpriteBehaviour)
+        self.component_3 = Mock(spec=SpriteBehaviour)
+        self.sprite.components.append(self.component_1)
+        self.sprite.components.append(self.component_2)
+        self.sprite.components.append(self.component_3)
 
     def test_should_add_sprite_to_group_on_creation(self):
         # given when
@@ -35,161 +53,87 @@ class XPGESpriteTest(TestCase):
         self.assertEqual(0, sprite.groups().index(group1))
         self.assertEqual(1, sprite.groups().index(group2))
 
-    def test_should_update_single_component(self):
-        # given
-        component_mock = Mock(spec=SpriteBehaviour)
-        sprite = XPGESprite(None)
-        sprite.components.append(component_mock)
-
+    def test_should_update_components(self):
         # when
-        sprite.update()
+        self.sprite.update()
 
         # then
-        component_mock.on_update.assert_called_once()
+        self.component_1.on_update.assert_called_once()
+        self.component_2.on_update.assert_called_once()
+        self.component_3.on_update.assert_called_once()
 
-    def test_should_update_many_component(self):
-        # given
-        component_mock_1 = Mock(spec=SpriteBehaviour)
-        component_mock_2 = Mock(spec=SpriteBehaviour)
-        component_mock_3 = Mock(spec=SpriteBehaviour)
-
-        sprite = XPGESprite(None)
-        sprite.components.append(component_mock_1)
-        sprite.components.append(component_mock_2)
-        sprite.components.append(component_mock_3)
-
+    def test_should_call_handle_event_on_components(self):
         # when
-        sprite.update()
+        self.sprite.handle_event(TEST_USEREVENT)
 
         # then
-        component_mock_1.on_update.assert_called_once()
-        component_mock_2.on_update.assert_called_once()
-        component_mock_3.on_update.assert_called_once()
-
-    def test_should_call_handle_event_on_single_component(self):
-        # given
-        event = Event(USEREVENT, dict())
-        component = Mock(spec=SpriteBehaviour)
-        sprite = XPGESprite(None)
-        sprite.components.append(component)
-
-        # when
-        sprite.handle_event(event)
-
-        # then
-        component.on_handle_event.assert_called_once_with(event)
-
-    def test_should_call_handle_event_on_many_components(self):
-        # given
-        event = Event(USEREVENT, dict())
-        component_mock_1 = Mock(spec=SpriteBehaviour)
-        component_mock_2 = Mock(spec=SpriteBehaviour)
-        component_mock_3 = Mock(spec=SpriteBehaviour)
-
-        sprite = XPGESprite(None)
-        sprite.components.append(component_mock_1)
-        sprite.components.append(component_mock_2)
-        sprite.components.append(component_mock_3)
-
-        # when
-        sprite.handle_event(event)
-
-        # then
-        component_mock_1.on_handle_event.assert_called_once_with(event)
-        component_mock_2.on_handle_event.assert_called_once_with(event)
-        component_mock_3.on_handle_event.assert_called_once_with(event)
+        self.component_1.on_handle_event.assert_called_once_with(TEST_USEREVENT)
+        self.component_2.on_handle_event.assert_called_once_with(TEST_USEREVENT)
+        self.component_3.on_handle_event.assert_called_once_with(TEST_USEREVENT)
 
     def test_should_set_sprite_focus_to_true(self):
         # given
-        sprite = XPGESprite(None)
-        sprite.rect = Mock(spec=Rect)
-        sprite.rect.collidepoint = Mock(return_value=True)
+        self.sprite.rect = Mock(spec=Rect)
+        self.sprite.rect.collidepoint = Mock(return_value=True)
 
         # when
-        sprite.handle_event(TEST_MOUSEMOTION_EVENT_WITH_POS_INSIDE_SPRITE)
+        self.sprite.handle_event(TEST_MOUSEMOTION_EVENT_WITH_POS_INSIDE_SPRITE)
 
         # then
-        sprite.rect.collidepoint.assert_called_once_with(MOUSE_POS)
-        self.assertTrue(sprite.focus)
+        self.sprite.rect.collidepoint.assert_called_once_with(MOUSE_POS_INSIDE_SPRITE)
+        self.assertTrue(self.sprite.focus)
 
     def test_should_set_sprite_focus_to_false(self):
         # given
-        sprite = XPGESprite(None)
-        sprite.rect = Mock(spec=Rect)
-        sprite.rect.collidepoint = Mock(return_value=False)
+        self.sprite.rect = Mock(spec=Rect)
+        self.sprite.rect.collidepoint = Mock(return_value=False)
 
         # when
-        sprite.handle_event(TEST_MOUSEMOTION_EVENT_WITH_POS_INSIDE_SPRITE)
+        self.sprite.handle_event(TEST_MOUSEMOTION_EVENT_WITH_POS_INSIDE_SPRITE)
 
         # then
-        sprite.rect.collidepoint.assert_called_once_with(MOUSE_POS)
-        self.assertFalse(sprite.focus)
+        self.sprite.rect.collidepoint.assert_called_once_with(MOUSE_POS_INSIDE_SPRITE)
+        self.assertFalse(self.sprite.focus)
 
     def test_should_not_change_focus_when_take_focus_is_false(self):
         # given
-        sprite = XPGESprite(None)
-        sprite.take_focus = False
-        sprite.rect = Mock(spec=Rect)
-
-        mouse_pos = (50, 50)
-        event = Event(MOUSEMOTION, {'pos': mouse_pos})
+        self.sprite.take_focus = False
+        self.sprite.rect = Mock(spec=Rect)
 
         # when
-        sprite.handle_event(event)
+        self.sprite.handle_event(TEST_MOUSEMOTION_EVENT_WITH_POS_INSIDE_SPRITE)
 
         # then
-        sprite.rect.collidepoint.assert_not_called()
-        self.assertFalse(sprite.focus)
+        self.sprite.rect.collidepoint.assert_not_called()
+        self.assertFalse(self.sprite.focus)
 
     def test_should_call_on_hover_on_components(self):
-        # given
-        sprite = XPGESprite(None)
-        sprite.rect = Rect(0, 0, 100, 100)
-        component_1 = Mock(spec=SpriteBehaviour)
-        component_2 = Mock(spec=SpriteBehaviour)
-        component_3 = Mock(spec=SpriteBehaviour)
-        sprite.components.append(component_1)
-        sprite.components.append(component_2)
-        sprite.components.append(component_3)
-
         # when
-        sprite.handle_event(TEST_MOUSEMOTION_EVENT_WITH_POS_INSIDE_SPRITE)
+        self.sprite.handle_event(TEST_MOUSEMOTION_EVENT_WITH_POS_INSIDE_SPRITE)
 
         # then
-        component_1.on_hover.assert_called_once()
-        component_2.on_hover.assert_called_once()
-        component_3.on_hover.assert_called_once()
+        self.component_1.on_hover.assert_called_once()
+        self.component_2.on_hover.assert_called_once()
+        self.component_3.on_hover.assert_called_once()
 
     def test_should_call_on_hover_exit_on_components(self):
         # given
-        sprite = XPGESprite(None)
-        sprite.rect = Rect(0, 0, 100, 100)
-        sprite.focus = True
-        component_1 = Mock(spec=SpriteBehaviour)
-        component_2 = Mock(spec=SpriteBehaviour)
-        component_3 = Mock(spec=SpriteBehaviour)
-        sprite.components.append(component_1)
-        sprite.components.append(component_2)
-        sprite.components.append(component_3)
+        self.sprite.focus = True
 
         # when
-        sprite.handle_event(TEST_MOUSEMOTION_EVENT_WITH_POS_OUTSIDE_SPRITE)
+        self.sprite.handle_event(TEST_MOUSEMOTION_EVENT_WITH_POS_OUTSIDE_SPRITE)
 
         # then
-        component_1.on_hover_exit.assert_called_once()
-        component_2.on_hover_exit.assert_called_once()
-        component_3.on_hover_exit.assert_called_once()
+        self.component_1.on_hover_exit.assert_called_once()
+        self.component_2.on_hover_exit.assert_called_once()
+        self.component_3.on_hover_exit.assert_called_once()
 
     def test_should_draw_sprite_onto_surface(self):
         # given
-        sprite = XPGESprite(None)
-        sprite.rect = Rect(0, 0, 100, 100)
-        sprite.image = Surface((100, 100))
-
         surface = Mock(spec=Surface)
 
         # when
-        sprite.draw(surface)
+        self.sprite.draw(surface)
 
         # then
-        surface.blit.assert_called_once_with(sprite.image, sprite.rect.topleft)
+        surface.blit.assert_called_once_with(self.sprite.image, (SPRITE_X, SPRITE_Y))
