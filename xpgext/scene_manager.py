e0efe42157a769e36a318c9c2860752581df7192
Artq99 <amatracki@gmail.com>
2018-08-22 11:47:27 +0200

    remove the class SceneManagerBase

--- xpgext/scene_manager.py
+++ xpgext/scene_manager.py
@@ -10,8 +10,19 @@ class SceneLoadingError(Exception):
     """Raised when loading a scene was unsuccessful."""
 
 
-class SceneManagerBase:
-    """Abstract base class for all the scene manager types."""
+class SimpleSceneManager:
+    """
+    The class managing the scenes of the game.
+
+    It governs registering and loading of the scenes and the execution of the methods of the currently loaded scene.
+    This is the most basic version of such class that provides convenient defaults. If you want to alter the execution
+    of the whole game, the methods of this class should be overridden.
+    """
+
+    def __init__(self):
+        self._scenes = dict()
+        self._current_scene = None
+        self._sprites = list()
 
     def register_scene(self, scene, name):
         """
@@ -23,6 +34,10 @@ class SceneManagerBase:
         :type name: str
         """
 
+        if name in self._scenes.keys():
+            raise SceneRegisteringError(SCENE_ALREADY_REGISTERED_.format(name))
+        self._scenes[name] = scene
+
     def load_scene(self, name):
         """
         Load a previously registered scene.
@@ -31,60 +46,6 @@ class SceneManagerBase:
         :type name: str
         """
 
-    def draw(self, surface):
-        """
-        Draw all the scene elements on the given surface.
-
-        :param surface: the pygame main surface
-        :type surface: pygame.Surface
-        """
-
-    def handle_event(self, event):
-        """
-        Pass the event to each of the scene elements until one of them handles the event.
-
-        :param event: event to handle
-        :type event: pygame.event.Event
-        """
-
-    def update(self):
-        """
-        Update all the scene elements. Called every frame.
-        """
-
-    def find_by_name(self, name):
-        """
-        Find elements of the given name.
-
-        :param name: name of the elements to find
-        :type name: str
-        :return: list of elements of the given name
-        :rtype: list
-        """
-
-    def get_by_name(self, name):
-        """
-        Get the first element whose name matches the given one.
-
-        :param name: name to check
-        :type name: str
-        :return: element
-        """
-
-
-class SimpleSceneManager(SceneManagerBase):
-
-    def __init__(self):
-        self._scenes = dict()
-        self._current_scene = None
-        self._sprites = list()
-
-    def register_scene(self, scene, name):
-        if name in self._scenes.keys():
-            raise SceneRegisteringError(SCENE_ALREADY_REGISTERED_.format(name))
-        self._scenes[name] = scene
-
-    def load_scene(self, name):
         try:
             self._current_scene = self._scenes[name](self)
         except KeyError:
@@ -98,26 +59,61 @@ class SimpleSceneManager(SceneManagerBase):
                     component.on_scene_loaded()
 
     def draw(self, surface):
+        """
+        Draw all the scene elements on the given surface.
+
+        :param surface: the pygame main surface
+        :type surface: pygame.Surface
+        """
+
         surface.fill((0, 0, 0))
         for sprite in reversed(self._sprites):
             sprite.draw(surface)
 
     def handle_event(self, event):
+        """
+        Pass the event to each of the scene elements until one of them handles the event.
+
+        :param event: event to handle
+        :type event: pygame.event.Event
+        """
+
         for sprite in reversed(self._sprites):
             if sprite.handle_event(event):
                 break
 
     def update(self):
+        """
+        Update all the scene elements. Called every frame.
+        """
+
         for sprite in reversed(self._sprites):
             sprite.update()
 
     def find_by_name(self, name):
+        """
+        Find elements of the given name.
+
+        :param name: name of the elements to find
+        :type name: str
+        :return: list of elements of the given name
+        :rtype: list
+        """
+
         result = list()
         for sprite in self._sprites:
             result.extend(sprite.find_by_name(name))
         return result
 
     def get_by_name(self, name):
+        """
+        Get the first element whose name matches the given one.
+
+        :param name: name to check
+        :type name: str
+        :return: element
+        """
+
         sprites = self.find_by_name(name)
         if len(sprites) == 0:
             return None
